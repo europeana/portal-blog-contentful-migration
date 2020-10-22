3cfb85ce908457352953b27f7496faad3066a7a8
Richard Doe <richard.doe@rwdit.net>
2020-10-22 17:21:44 +0100

    Migrate media referenced in content

--- src/actions/attachments.js
+++ src/actions/attachments.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const axios = require('axios');
+
 const { mysqlClient, contentfulManagement } = require('../support/config');
 const { assetExists, cacheAssetMap } = require('./assets');
 const { LangMap, pad, hashedSysId } = require('../support/utils');
@@ -28,22 +30,27 @@ const migrateAttachment = async(post) => {
     return;
   }
 
-  try {
-    // Assets may not be published without a title. Fallback to file name.
-    const fileName = guid.split('/').pop();
-    const title = post.post_title || fileName;
+  await createAndPublish(assetId, guid, post.post_title, post.post_mime_type);
+};
 
-    const assetData = {
-      fields: {
-        title: new LangMap(title),
-        file: new LangMap({
-          contentType: post.post_mime_type,
-          fileName,
-          upload: guid
-        })
-      }
-    };
-    const asset = await contentfulManagement.environment.createAssetWithId(assetId, assetData);
+const createAndPublish = async(id, url, title, contentType) => {
+  // Assets may not be published without a title. Fallback to file name.
+  const fileName = url.split('/').pop();
+  title = title || fileName;
+
+  const assetData = {
+    fields: {
+      title: new LangMap(title),
+      file: new LangMap({
+        contentType,
+        fileName,
+        upload: url
+      })
+    }
+  };
+
+  try {
+    const asset = await contentfulManagement.environment.createAssetWithId(id, assetData);
 
     const processedAsset = await asset.processForAllLocales();
     if (process.env['SKIP_ASSET_PUBLISH_AWAIT'] === '1') {
@@ -52,9 +59,9 @@ const migrateAttachment = async(post) => {
       await processedAsset.publish();
     }
 
-    pad.log(`[NEW] <${guid}> ${asset.sys.id}`);
+    pad.log(`[NEW] <${url}> ${asset.sys.id}`);
   } catch (e) {
-    pad.log(`[ERROR] <${guid}> ${e}`);
+    pad.log(`[ERROR] <${url}> ${e}`);
   }
 };
 
@@ -70,9 +77,13 @@ const migrateAttachments = async() => {
   }
 };
 
-const detectMissingGeneratedMedia = async() => {
+/**
+ * Scrape all post bodies for links to uploads, check if they have already been
+ * migrated, if not, handle them.
+ */
+const migrateMediaReferencedInContent = async() => {
   const result = await mysqlClient.connection.execute(`
-    SELECT ID, post_content FROM wp_posts WHERE post_type='post'
+    SELECT ID, post_content FROM wp_posts WHERE post_type='post' ORDER BY post_date ASC
   `);
 
   for (const post of result[0]) {
@@ -80,14 +91,25 @@ const detectMissingGeneratedMedia = async() => {
     const matches = post.post_content.matchAll(pattern);
     for (const match of matches) {
       const url = match[1];
-      const assetId = hashedSysId(accessibleGuid(match[1]));
+      const guid = accessibleGuid(url);
+      const assetId = hashedSysId(guid);
       if (await assetExists(assetId)) {
-        pad.log(`[EXISTS] <${url}> ${assetId}`);
+        pad.log(`[EXISTS] <${guid}> ${assetId}`);
       } else {
-        pad.log(`[MISSING] <${url}> ${assetId}`);
+        try {
+          const contentType = await getContentType(guid);
+          await createAndPublish(assetId, guid, null, contentType);
+        } catch (e) {
+          pad.log(`[ERROR] <${guid}> ${e.message}`);
+        }
       }
     }
   }
+};
+
+const getContentType = async(url) => {
+  const response = await axios.head(url);
+  return response.headers['content-type'];
 };
 
 const cli = async() => {
@@ -95,8 +117,8 @@ const cli = async() => {
   await mysqlClient.connect();
 
   await migrateAttachments();
-  // await cacheAssetMap();
-  // await detectMissingGeneratedMedia();
+  await cacheAssetMap();
+  await migrateMediaReferencedInContent();
 
   await mysqlClient.connection.end();
 };
